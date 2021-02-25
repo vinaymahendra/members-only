196b0876ff684a608e9ffa2e06d713ffe02958d0
vinay.mahendra <vinay.mahendra@dynamictalentpartners.com>
2021-02-25 13:25:24 -0700

    added calendar import to index page

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import netlifyAuth from '../netlifyAuth.js'
 
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Calendar from '@components/Calendar'
 
 export default function Home() {
   let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
